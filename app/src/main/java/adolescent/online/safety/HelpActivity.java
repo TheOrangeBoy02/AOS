61fc6458de7a573b8c486cc397b06a6e15fd5bcb
ORANGEcode <83740910+TheOrangeBoy02@users.noreply.github.com>
2021-12-08 17:43:35 +0200

    AA

---
+++ app/src/main/java/adolescent/online/safety/HelpActivity.java
@@ -0,0 +1,15 @@
+package adolescent.online.safety;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.TextView;
+
+public class HelpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_instructions);
+    }
+}
